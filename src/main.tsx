3b0b663e8a424bab388b0dd03ce2bcfbf48f39ca
justanotheratom <sanketpatel.1805@gmail.com>
2024-05-22 10:50:57 -0700

    Cosmetic change to trigger a vercel deployment

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import "./index.css";
-
 import { inject } from '@vercel/analytics';
  
 inject();
